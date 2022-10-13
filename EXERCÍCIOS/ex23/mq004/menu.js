6bcf63749b9e65072d12a225d532860c48731fcb
Thiago Da silva Gpmes <103701494+Thiagodsilvagomes@users.noreply.github.com>
2022-10-12 21:33:24 -0300

    Menu Mobile

---
+++ EXERCÍCIOS/ex23/mq004/menu.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById('menu')
+
+function clickMenu(){
+   if (menu.style.display == 'block'){
+        menu.style.display = 'none'
+   }else{
+        menu.style.display = 'block'
+   }
+}
+
