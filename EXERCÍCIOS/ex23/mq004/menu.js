d7ead332b2727c5d65dfe0fd56476b6422f97a5b
Thiago Da silva Gpmes <103701494+Thiagodsilvagomes@users.noreply.github.com>
2022-10-13 21:37:33 -0300

    Menu Mobile

--- EXERCÍCIOS/ex23/mq004/menu.js
+++ EXERCÍCIOS/ex23/mq004/menu.js
@@ -1,5 +1,14 @@
 let menu = document.getElementById('menu')
 
+function mudouTamanho(){
+    if (window.innerWidth >= 768){
+          menu.style.display = 'block'
+    }else{
+          menu.style.display = 'none'
+    }
+  } 
+
+
 function clickMenu(){
    if (menu.style.display == 'block'){
         menu.style.display = 'none'
